3afe1a318aaf3a5fdc1af0a25a770e3b02e494b6
vargab-dev <varga95bence@icloud.com>
2025-03-03 23:43:53 +0100

    react fixed, gh-pages works

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Üdv a portfólió weboldalamon!</h1>
+      <p>Hamarosan itt lesznek az automata tesztek és projektek.</p>
+    </div>
+  );
+}
+
+export default App;
